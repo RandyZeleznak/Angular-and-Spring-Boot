ba19c001bd696e789b7f913b26cacd997bf6d90e
Randy <RandyZeleznak@gmail.com>
2020-08-31 11:08:55 -0500

    Not outputting - 082120:1100

---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css'],
+})
+export class salesPersonListComponent implements OnInit {
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Joe', 'Bonamassa', 'joe.bonamassa@gmail.com', 2500),
+    new SalesPerson('Eric', 'Johnson', 'eric.johnson@gmail.com', 3000),
+    new SalesPerson('Rik', 'Emmett', 'rik.emmett@gmail.com', 1500),
+    new SalesPerson('Steve', 'Morse', 'steve.morse@gmail.com', 4000),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
