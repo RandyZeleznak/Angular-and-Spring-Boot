4f94f0a42adb5713a28d81c1c835a0b2f6795255
Randy <RandyZeleznak@gmail.com>
2020-11-17 16:17:54 -0600

    Section #14 Completed - 111720:1615

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/zsquared/ecommerce/config/MyDataRestConfig.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/zsquared/ecommerce/config/MyDataRestConfig.java
@@ -2,13 +2,30 @@ package com.zsquared.ecommerce.config;
 
 import com.zsquared.ecommerce.entity.Product;
 import com.zsquared.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager){
+        entityManager= theEntityManager;
+    }
+
+
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 
@@ -29,6 +46,29 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metadata,httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metadata,httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
+
+        // Call to the internal helper method
+        exposeIds(config);
+
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // expose entity Ids
+
+        // get list of entity  classes from the EntityManager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        //Create Array of entitytypes
+        List<Class> entityClasses = new ArrayList<>();
+
+        // Load Entity Types into entities
+        for(EntityType tempEntityType : entities){
+            entityClasses.add((tempEntityType.getJavaType()));
+        }
+        
+        // Expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
 
 
 
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-product-list',
@@ -11,6 +12,8 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductListComponent implements OnInit {
   products: Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
+  searchMode: boolean;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) {}
@@ -23,22 +26,48 @@ export class ProductListComponent implements OnInit {
 
   listProducts() {
 
-    // check to see if the 'id' parameter is available
-    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
-    if(hasCategoryId){
-      //get 'id' string and convert to number
-      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
-    } else{
-      //set catergoryId to default of '1'
-      this.currentCategoryId = 1;
+    if(this.searchMode){
+      this.handleSearchProducts();
+    }else{
+      this.handleListProducts();
     }
+  }
 
-  // Now get products for captured 'id'
-    this.productService.getProductList(this.currentCategoryId).subscribe(
+  handleSearchProducts(){
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    // Now search for products containing the given keyword
+    this.productService.searchProducts(theKeyword).subscribe(
       data => {
-      this.products = data;
+        this.products = data;
       }
     )
+  }
+
+  handleListProducts(){
+// check to see if the 'id' parameter is available
+const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    
+
+if(hasCategoryId){
+  //get 'id' string and convert to number
+  this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+  //get Category Name for string
+  this.currentCategoryName = this.route.snapshot.paramMap.get('name');
+} else{
+  //set catergoryId to default of '1'
+  this.currentCategoryId = 1;
+  this.currentCategoryName = "Books";
+}
+
+// Now get products for captured 'id'
+this.productService.getProductList(this.currentCategoryId).subscribe(
+  data => {
+  this.products = data;
+  }
+)
   }
 }
